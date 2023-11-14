92e2004465350a0b3efee32dbba7a05606506b50
JuanP <juanpajueloch@gmail.com>
2023-11-14 12:58:26 -0500

    loadDB actualizado

--- src/utils/randomDate.js
+++ src/utils/randomDate.js
@@ -1,11 +1,11 @@
-const randomDate = () => {
+const randomDate = (fecha = "2023-01-05") => {//"2023-01-01"
     const hoy = new Date();
     //const dosMesesAtras = new Date();
-    const eneroDate = new Date("2023-01-01");
+    const initDate = new Date(fecha);
 
     //dosMesesAtras.setMonth(hoy.getMonth() - 2);
   
-    const tiempoAleatorio = (1-((Math.random())**2)) * (hoy.getTime() - eneroDate.getTime()) + eneroDate.getTime();
+    const tiempoAleatorio = (1-((Math.random())**1)) * (hoy.getTime() - initDate.getTime()) + initDate.getTime();
     const fechaAleatoria = new Date(tiempoAleatorio);
   
     return fechaAleatoria;
